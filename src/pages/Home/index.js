893e56972d73dbff4b11b461f2b706ae42b178d6
Louis Gerber <louisgerber004@gmail.com>
2023-09-14 15:50:01 +0200

    fixed eventcard //showing last event properly

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,8 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const last = useData()
-  // const last = data?.data.events[0]
+  const data = useData()
+  const last = data?.data?.events[0]
   return <>
     <header>
       <Menu />
